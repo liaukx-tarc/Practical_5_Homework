94331bb1ea3c2b76cd56a00be0bd95005e07436f
liaukx-tarc <liaukx@student.tarc.edu.my>
2020-07-14 16:31:57 +0800

    Practical 4.2

--- app/src/main/java/com/example/android/droidcafe/OrderActivity.java
+++ app/src/main/java/com/example/android/droidcafe/OrderActivity.java
@@ -18,6 +18,7 @@ package com.example.android.droidcafe;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -95,5 +96,19 @@ import android.widget.Toast;
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+
+    public void showDatePicker(View view)
+    {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(),getString(R.string.date_pircker));
+    }
+
+    public void processDatePickerResult(int year, int month, int day) {
+        String month_string = Integer.toString(month+1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        String dateMessage = (month_string + "/" + day_string + "/" + year_string);
+        displayToast(getString(R.string.date) + dateMessage);
     }
 }
